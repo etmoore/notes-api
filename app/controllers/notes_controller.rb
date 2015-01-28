eabfc2dce07cce1de800d92598799abfd555c5b3
Evan Moore <etmoore@gmail.com>
2015-01-28 10:02:07 -0700

    CRUD for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,19 +1,37 @@
 class NotesController < ApplicationController
 
+  before_action :set_note, only: [:show, :update, :destroy]
+
   def index
     @notes = Note.all
     render json: @notes
   end
 
   def show
+    @note = Note.find(params[:id])
+    render json: @note
   end
 
   def create
+    Note.create(note_params)
   end
 
   def update
+    @note.update(note_params)
   end
 
   def destroy
+    @note.destroy
   end
+
+  private
+
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  get '/notes' => 'notes#index'
+  get     '/notes'     => 'notes#index'
+  post    '/notes'     => 'notes#create'
+  get     '/notes/:id' => 'notes#show'
+  put     '/notes/:id' => 'notes#update'
+  delete  '/notes/:id' => 'notes#destroy'
 
 end
